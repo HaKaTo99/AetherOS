ef44c43e1141ef23f098be308de20ffe75ccf558
HaKaTo99 <hkrisnanto@gmail.com>
2026-03-31 11:58:43 +0700

    deps: vendor patched bitvec crate for reproducible workspace builds

---
+++ patches/bitvec/bitvec/src/lib.rs
@@ -0,0 +1,77 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(
+	debug_assertions,
+	allow(missing_docs, clippy::missing_docs_in_private_items)
+)]
+#![cfg_attr(
+	not(debug_assertions),
+	allow(missing_docs, clippy::missing_docs_in_private_items)
+)]
+#![deny(unconditional_recursion)]
+#![allow(
+	clippy::declare_interior_mutable_const,
+	clippy::type_complexity,
+	unknown_lints
+)]
+#![allow(unexpected_cfgs)]
+#![allow(mismatched_lifetime_syntaxes)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[macro_use]
+mod devel;
+
+#[macro_use]
+pub mod macros;
+
+pub mod access;
+pub mod array;
+pub mod boxed;
+pub mod domain;
+pub mod field;
+pub mod index;
+pub mod mem;
+pub mod order;
+pub mod ptr;
+mod serdes;
+pub mod slice;
+pub mod store;
+pub mod vec;
+pub mod view;
+
+pub mod prelude {
+	pub use crate::{
+		array::BitArray,
+		bitarr,
+		bits,
+		field::BitField as _,
+		order::{
+			BitOrder,
+			LocalBits,
+			Lsb0,
+			Msb0,
+		},
+		ptr::{
+			BitPtr,
+			BitPtrRange,
+			BitRef,
+		},
+		slice::BitSlice,
+		store::BitStore,
+		view::{
+			AsBits,
+			AsMutBits,
+			BitView as _,
+			BitViewSized as _,
+		},
+		BitArr,
+	};
+	#[cfg(feature = "alloc")]
+	pub use crate::{
+		bitbox,
+		bitvec,
+		boxed::BitBox,
+		vec::BitVec,
+	};
+}
